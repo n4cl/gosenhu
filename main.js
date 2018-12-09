ae50ef503fb964eb8e1965ca0786811dd3b84a32
n4cl <devn4cl@gmail.com>
2018-12-10 02:52:49 +0900

    Add score sample

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var $ = require('vexflow');
+window.onload = function(){
+let vexflow = require('vexflow');
+vexflow = vexflow.Flow
+
+const div = document.getElementById('container');
+const renderer = new vexflow.Renderer(div, vexflow.Renderer.Backends.SVG);
+renderer.resize(500, 200);
+
+const context = renderer.getContext();
+const stave = new vexflow.Stave(10, 40, 400);
+
+stave.setContext(context).draw();
+};
