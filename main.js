ecbc8577ab18ae9075ee9dd8eac69b55ac3bef69
n4cl <devn4cl@gmail.com>
2018-12-11 02:22:06 +0900

    Add treble and bass

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ window.onload = function(){
 let vexflow = require('vexflow');
 vexflow = vexflow.Flow
 
-const div = document.getElementById('container');
-const renderer = new vexflow.Renderer(div, vexflow.Renderer.Backends.SVG);
-renderer.resize(500, 200);
+const treble = document.getElementById('treble');
+const base = document.getElementById('base');
 
-const context = renderer.getContext();
-const stave = new vexflow.Stave(10, 40, 400);
+const trebleRenderer = new vexflow.Renderer(treble, vexflow.Renderer.Backends.SVG);
+const baseRenderer = new vexflow.Renderer(base, vexflow.Renderer.Backends.SVG);
 
-stave.setContext(context).draw();
+trebleRenderer.resize(500, 150);
+baseRenderer.resize(500, 150);
+
+const trebleContext = trebleRenderer.getContext();
+const baseContext = baseRenderer.getContext();
+
+const trebleStave = new vexflow.Stave(10, 40, 400);
+const baseStave = new vexflow.Stave(10, 40, 400);
+
+trebleStave.addClef("treble").addTimeSignature("4/4");
+trebleStave.setContext(trebleContext).draw();
+
+baseStave.addClef("bass").addTimeSignature("4/4");
+baseStave.setContext(baseContext).draw();
 };
