8cff62ed4245200ce757b991b3f35060bcfa5af7
n4cl <devn4cl@gmail.com>
2018-12-31 06:01:19 +0900

    Fix format

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-window.onload = function(){
-let vexflow = require('vexflow');
-vexflow = vexflow.Flow
+window.onload = function () {
+  let vexflow = require("vexflow");
+  vexflow = vexflow.Flow;
 
-const treble = document.getElementById('treble');
-const base = document.getElementById('base');
+  const treble = document.getElementById("treble");
+  const base = document.getElementById("base");
 
-const trebleRenderer = new vexflow.Renderer(treble, vexflow.Renderer.Backends.SVG);
-const baseRenderer = new vexflow.Renderer(base, vexflow.Renderer.Backends.SVG);
+  const trebleRenderer = new vexflow.Renderer(treble, vexflow.Renderer.Backends.SVG);
+  const baseRenderer = new vexflow.Renderer(base, vexflow.Renderer.Backends.SVG);
 
-trebleRenderer.resize(500, 150);
-baseRenderer.resize(500, 150);
+  trebleRenderer.resize(500, 150);
+  baseRenderer.resize(500, 150);
 
-const trebleContext = trebleRenderer.getContext();
-const baseContext = baseRenderer.getContext();
+  const trebleContext = trebleRenderer.getContext();
+  const baseContext = baseRenderer.getContext();
 
-const trebleStave = new vexflow.Stave(10, 40, 400);
-const baseStave = new vexflow.Stave(10, 40, 400);
+  const trebleStave = new vexflow.Stave(10, 40, 400);
+  const baseStave = new vexflow.Stave(10, 40, 400);
 
-trebleStave.addClef("treble").addTimeSignature("4/4");
-trebleStave.setContext(trebleContext).draw();
+  trebleStave.addClef("treble").addTimeSignature("4/4");
+  trebleStave.setContext(trebleContext).draw();
 
-baseStave.addClef("bass").addTimeSignature("4/4");
-baseStave.setContext(baseContext).draw();
+  baseStave.addClef("bass").addTimeSignature("4/4");
+  baseStave.setContext(baseContext).draw();
 };
